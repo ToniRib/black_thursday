a4518211cfbc16681cac6c5e5ee9575007ea6034
Toni Rib <tonimarierib@gmail.com>
2018-02-17 16:44:18 -0700

    Refactor parser specs to add method names

--- spec/csv/item_parser_spec.rb
+++ spec/csv/item_parser_spec.rb
@@ -5,40 +5,42 @@ module Csv
   describe ItemParser do
     let(:filename) { './spec/data/items.csv' }
 
-    it 'creates an Item for each non-header line' do
-      items = described_class.parse(filename)
+    describe '.parse' do
+      it 'creates an Item for each non-header line' do
+        items = described_class.parse(filename)
 
-      expect(items.length).to eq 3
-    end
+        expect(items.length).to eq 3
+      end
 
-    it 'creates the correct Items' do
-      items = described_class.parse(filename)
+      it 'creates the correct Items' do
+        items = described_class.parse(filename)
 
-      expect(items[0].name).to eq 'Romantic coffee service &quot;Rüdesheimer Kaffee&quot;'
-      expect(items[0].description).to start_with 'This is a romantic coffee service, made'
-      expect(items[0].unit_price).to eq BigDecimal.new(8000, 4)
-      expect(items[0].created_at).to eq DateTime.parse('2016-01-11 12:17:26 UTC')
-      expect(items[0].updated_at).to eq DateTime.parse('1988-06-26 02:51:02 UTC')
+        expect(items[0].name).to eq 'Romantic coffee service &quot;Rüdesheimer Kaffee&quot;'
+        expect(items[0].description).to start_with 'This is a romantic coffee service, made'
+        expect(items[0].unit_price).to eq BigDecimal.new(8000, 4)
+        expect(items[0].created_at).to eq DateTime.parse('2016-01-11 12:17:26 UTC')
+        expect(items[0].updated_at).to eq DateTime.parse('1988-06-26 02:51:02 UTC')
 
-      expect(items[1].name).to eq 'Custom Listing: Twenty booklets R.H. Life, Biographical in nature, own publishing 2014.'
-      expect(items[1].description).to start_with 'Custom Listing: Twenty booklets R.H. Life'
-      expect(items[1].unit_price).to eq BigDecimal.new(100000, 4)
-      expect(items[1].created_at).to eq DateTime.parse('2016-01-11 17:41:20 UTC')
-      expect(items[1].updated_at).to eq DateTime.parse('1974-11-29 08:58:13 UTC')
+        expect(items[1].name).to eq 'Custom Listing: Twenty booklets R.H. Life, Biographical in nature, own publishing 2014.'
+        expect(items[1].description).to start_with 'Custom Listing: Twenty booklets R.H. Life'
+        expect(items[1].unit_price).to eq BigDecimal.new(100000, 4)
+        expect(items[1].created_at).to eq DateTime.parse('2016-01-11 17:41:20 UTC')
+        expect(items[1].updated_at).to eq DateTime.parse('1974-11-29 08:58:13 UTC')
 
-      expect(items[2].name).to eq 'Du Romantisme'
-      expect(items[2].description).to start_with 'Acrylique sur toile exécutée en 2015'
-      expect(items[2].unit_price).to eq BigDecimal.new(60000, 4)
-      expect(items[2].created_at).to eq DateTime.parse('2016-01-11 11:30:35 UTC')
-      expect(items[2].updated_at).to eq DateTime.parse('1986-12-21 16:16:32 UTC')
-    end
+        expect(items[2].name).to eq 'Du Romantisme'
+        expect(items[2].description).to start_with 'Acrylique sur toile exécutée en 2015'
+        expect(items[2].unit_price).to eq BigDecimal.new(60000, 4)
+        expect(items[2].created_at).to eq DateTime.parse('2016-01-11 11:30:35 UTC')
+        expect(items[2].updated_at).to eq DateTime.parse('1986-12-21 16:16:32 UTC')
+      end
 
-    it 'adds merchant_id to Items' do
-      items = described_class.parse(filename)
+      it 'adds merchant_id to Items' do
+        items = described_class.parse(filename)
 
-      expect(items[0].merchant_id).to eq 12334235
-      expect(items[1].merchant_id).to eq 12335128
-      expect(items[2].merchant_id).to eq 12334195
+        expect(items[0].merchant_id).to eq 12334235
+        expect(items[1].merchant_id).to eq 12335128
+        expect(items[2].merchant_id).to eq 12334195
+      end
     end
   end
 end
--- spec/csv/merchant_parser_spec.rb
+++ spec/csv/merchant_parser_spec.rb
@@ -5,21 +5,23 @@ module Csv
   describe MerchantParser do
     let(:filename) { './spec/data/merchants.csv' }
 
-    it 'creates a Merchant for each non-header line' do
-      merchants = described_class.parse(filename)
+    describe '.parse' do
+      it 'creates a Merchant for each non-header line' do
+        merchants = described_class.parse(filename)
 
-      expect(merchants.length).to eq 3
-    end
+        expect(merchants.length).to eq 3
+      end
 
-    it 'creates the correct Merchants' do
-      merchants = described_class.parse(filename)
+      it 'creates the correct Merchants' do
+        merchants = described_class.parse(filename)
 
-      expect(merchants[0].id).to eq 12334105
-      expect(merchants[0].name).to eq 'Shopin1901'
-      expect(merchants[1].id).to eq 12334112
-      expect(merchants[1].name).to eq 'Candisart'
-      expect(merchants[2].id).to eq 12334113
-      expect(merchants[2].name).to eq 'MiniatureBikez'
+        expect(merchants[0].id).to eq 12334105
+        expect(merchants[0].name).to eq 'Shopin1901'
+        expect(merchants[1].id).to eq 12334112
+        expect(merchants[1].name).to eq 'Candisart'
+        expect(merchants[2].id).to eq 12334113
+        expect(merchants[2].name).to eq 'MiniatureBikez'
+      end
     end
   end
 end
