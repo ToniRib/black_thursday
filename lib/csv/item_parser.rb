6683fc78f2f8e8ec7fda749d53e03132c8b89144
Toni Rib <tonimarierib@gmail.com>
2018-02-17 16:39:21 -0700

    Add Csv::ItemParser

---
+++ lib/csv/item_parser.rb
@@ -0,0 +1,18 @@
+require './lib/item'
+require 'csv'
+
+module Csv
+  class ItemParser
+    def self.parse(filename)
+      CSV.open(filename, { headers: true, header_converters: :symbol }).map do |row|
+        Item.new({
+          name: row[:name],
+          description: row[:description],
+          unit_price: BigDecimal.new(row[:unit_price].to_i, 4),
+          created_at: DateTime.parse(row[:created_at]),
+          updated_at: DateTime.parse(row[:updated_at]),
+        })
+      end
+    end
+  end
+end
