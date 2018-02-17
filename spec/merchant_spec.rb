6a82e6ec5edcea9e73ee46cb54ce09a770489fdf
Toni Rib <tonimarierib@gmail.com>
2018-02-17 16:02:44 -0700

    Add basic Merchant class

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
+require './lib/merchant'
 
 describe Merchant do
-  
+  subject { described_class.new({ id: 1, name: 'Etsy' }) }
+
+  it 'has a name' do
+    expect(subject.name).to eq 'Etsy'
+  end
+
+  it 'has an id' do
+    expect(subject.id).to eq 1
+  end
 end
