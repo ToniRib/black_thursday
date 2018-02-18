32a3f80253b3fd41140f6d7f0892483f7bd60f01
Toni Rib <tonimarierib@gmail.com>
2018-02-17 17:05:55 -0700

    Add MerchantRepository

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,24 @@
+class MerchantRepository
+  def initialize(merchants)
+    @merchants = merchants
+  end
+
+  def all
+    merchants
+  end
+
+  def find_by_id(id)
+    merchants.find { |merchant| merchant.id == id }
+  end
+
+  def find_by_name(name)
+    merchants.find { |merchant| merchant.name.downcase == name.downcase }
+  end
+
+  def find_all_by_name(name)
+    merchants.select { |merchant| merchant.name.downcase.include?(name.downcase) }
+  end
+
+  private
+  attr_reader :merchants
+end
---
+++ spec/merchant_repository_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+require './lib/merchant_repository'
+
+describe MerchantRepository do
+  let(:merchants) do
+    [
+      Merchant.new(id: 1, name: 'Etsy'),
+      Merchant.new(id: 2, name: 'Target'),
+    ]
+  end
+
+  subject { described_class.new(merchants) }
+
+  describe '#all' do
+    it 'returns an array of Merchants' do
+      expect(subject.all.map(&:class).uniq).to eq [Merchant]
+    end
+  end
+
+  describe '#find_by_id' do
+    context 'when Merchant is found' do
+      it 'returns the correct Merchant' do
+        expect(subject.find_by_id(1)).to eq merchants[0]
+      end
+    end
+
+    context 'when Merchant cannot be found' do
+      it 'returns nil' do
+        expect(subject.find_by_id(1000)).to be_nil
+      end
+    end
+  end
+
+  describe '#find_by_name' do
+    context 'when exact Merchant name is found' do
+      it 'returns the correct Merchant' do
+        expect(subject.find_by_name('Target')).to eq merchants[1]
+      end
+    end
+
+    context 'when case insensitive match of name is found' do
+      it 'returns the correct Merchant' do
+        expect(subject.find_by_name('tARget')).to eq merchants[1]
+      end
+    end
+
+    context 'when Merchant cannot be found' do
+      it 'returns nil' do
+        expect(subject.find_by_name('Sports Authority')).to be_nil
+      end
+    end
+  end
+
+  describe '#find_all_by_name' do
+    let(:merchants) do
+      [
+        Merchant.new(id: 1, name: 'Etsy'),
+        Merchant.new(id: 2, name: 'Target'),
+        Merchant.new(id: 3, name: 'Super Target'),
+      ]
+    end
+
+    context 'when merchant names match any part' do
+      it 'returns the correct Merchants' do
+        expect(subject.find_all_by_name('Target')).to match_array [merchants[1], merchants[2]]
+      end
+    end
+
+    context 'when name matches case insensitively' do
+      it 'returns the correct Merchants' do
+        expect(subject.find_all_by_name('tarGET')).to match_array [merchants[1], merchants[2]]
+      end
+    end
+
+    context 'when no names match' do
+      it 'an empty array' do
+        expect(subject.find_all_by_name('bananas')).to be_empty
+      end
+    end
+  end
+end
