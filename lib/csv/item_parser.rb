f6f36ed33aeadb4fc739d73d330fb69e20cf90b1
Toni Rib <tonimarierib@gmail.com>
2018-02-17 17:56:22 -0700

    Change DateTime to Time for spec harness

--- lib/csv/item_parser.rb
+++ lib/csv/item_parser.rb
@@ -1,5 +1,6 @@
 require_relative '../item'
 require 'csv'
+require 'time'
 
 module Csv
   class ItemParser
@@ -10,8 +11,8 @@ module Csv
           name: row[:name],
           description: row[:description],
           unit_price: BigDecimal.new(row[:unit_price].to_i, 4),
-          created_at: DateTime.parse(row[:created_at]),
-          updated_at: DateTime.parse(row[:updated_at]),
+          created_at: Time.parse(row[:created_at]),
+          updated_at: Time.parse(row[:updated_at]),
         }).tap do |item|
           item.merchant_id = row[:merchant_id].to_i
         end
